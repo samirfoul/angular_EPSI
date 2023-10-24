6095d7010b33490b0af2bedff92dbacc54013e7c
samirfoul <foul_s@hotmail.fr>
2023-10-24 16:12:47 +0200

    My first commit

---
+++ src/app/Model/tennis-player.ts
@@ -0,0 +1,6 @@
+export class ModelTennisPlayer {
+  public id = 1;
+  public LastName = "Sampras";
+  public Firstname = "Pete";
+  public rank = 5;
+}
---
+++ src/app/tennis-player/tennis-player.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit, } from '@angular/core';
+import { ModelTennisPlayer } from '../Model/tennis-player';
+
+
+
+@Component({
+  selector: 'app-tennis-player',
+  templateUrl: './tennis-player.component.html',
+  styleUrls: ['./tennis-player.component.css']
+})
+export class TennisPlayerComponent implements OnInit {
+
+  // proprieté ( les attributtes )
+  public player = new ModelTennisPlayer;
+  public players: ModelTennisPlayer[] = [];
+
+  constructor() { }
+
+  // onclick methode
+  public onClick() {
+    let NewPlayer = new ModelTennisPlayer()
+    this.players.push(this.player);
+
+
+  }
+
+  ngOnInit(): void {
+  }
+
+}
